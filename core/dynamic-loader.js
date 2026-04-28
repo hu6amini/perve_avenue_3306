2d5b139a800e9668b19e8fcb75e0ec3e71f97b4a
jr <89549621+hu6amini@users.noreply.github.com>
2026-04-28 17:51:04 +0300

    Update dynamic-loader.js

--- core/dynamic-loader.js
+++ core/dynamic-loader.js
@@ -73,7 +73,7 @@ document.head.appendChild(instantPagePreload);
     // ============================================
     // 5. ENHANCEMENT MODULES
     // ============================================
-    "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@52caa4f/modules/posts.js",
+    "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@560210e/modules/posts.js",
     
     // ============================================
     // 6. MAIN ENHANCER (MUST BE LAST)

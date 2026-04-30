30b8331c2f96e846dd84ef681d98e2003c33b800
jr <89549621+hu6amini@users.noreply.github.com>
2026-04-30 15:14:30 +0300

    Change font weight from 400 to 300 for avatars

--- modules/posts.js
+++ modules/posts.js
@@ -175,7 +175,7 @@ function generateDiceBearAvatar(username, userId) {
         '&backgroundColor=' + backgroundColor +
         '&size=' + CONFIG.AVATAR_SIZE +
         '&fontSize=62' + // Set to 62% of the total size
-        '&fontWeight=400' +
+        '&fontWeight=300' +
         '&radius=50';
 }
 

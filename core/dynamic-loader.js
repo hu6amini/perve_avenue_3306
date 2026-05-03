17a1cd73da0fb224ba26acf43a81bbfb54723d84
jr <89549621+hu6amini@users.noreply.github.com>
2026-05-03 16:02:33 +0300

    Update dynamic-loader.js

--- core/dynamic-loader.js
+++ core/dynamic-loader.js
@@ -85,7 +85,7 @@ const SCRIPT_URLS = [
     // Modules (each will wait for forum-observer-ready internally)
     "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@f29c780/modules/media-dimensions.js",
     "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@5974f8e/modules/twemoji.js",
-    "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@371d705/modules/posts.js",
+    "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@4cc2f88/modules/posts.js",
     "https://cdn.jsdelivr.net/gh/hu6amini/perve_avenue@951ae91/modules/modals.js",
     
     // Main enhancer (last)
